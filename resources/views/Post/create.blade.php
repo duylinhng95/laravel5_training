c95c99ec80f6176fbec109ed699a1bd0858949b5
Duy Linh Nguyen <duylinhng95@gmail.com>
2019-01-04 11:37:26 +0700

    Add create file

--- resources/views/Post/create.blade.php
+++ resources/views/Post/create.blade.php
@@ -1,25 +1,34 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Post</title>
-</head>
-<body>
+@extends('Post.layout')
+@section('title')
+    Create Post
+@endsection
+@section('content')
+    <div class="card-header">
+        <h2><i class="fa fa-plus"></i> Create Post</h2>
+    </div>
     <form action="{{url('post')}}" method="post">
-        {{csrf_field()}}
-        <label for="title">Title</label>
-        <input type="text" name="title">
-
-        <label for="description">Description</label>
-        <input type="text" name="description">
-
-        <label for="title">Content</label>
-        <textarea name="content" id="" cols="30" rows="10"></textarea>
-
-        <button type="submit">Submit</button>
+        <div class="card-body">
+            {{csrf_field()}}
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title"
+                       class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input type="text" name="description"
+                       class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="title">Content</label>
+                <textarea name="content" id="" cols="30" rows="10"
+                           class="form-control"></textarea>
+            </div>
+        </div>
+        <div class="card-footer">
+            <button type="submit" class="btn btn-outline-primary">Submit</button>
+            <a href="{{url('/post')}}" class="btn btn-outline-secondary">Cancel</a>
+        </div>
     </form>
-</body>
-</html>
+@endsection
+
