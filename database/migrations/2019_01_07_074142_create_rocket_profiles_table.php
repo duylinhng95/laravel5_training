d8b955453c245e2d197d58e47d54bb5aba924fb8
Duy Linh Nguyen <duylinhng95@gmail.com>
2019-01-07 14:53:59 +0700

    Create init database migrations

---
+++ database/migrations/2019_01_07_074142_create_rocket_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRocketProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rocket_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('auth_token');
+            $table->string('owner_id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rocket_profiles');
+    }
+}
