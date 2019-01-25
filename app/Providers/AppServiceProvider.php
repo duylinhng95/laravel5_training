e9011474866c0d75e36d3d48eea84d75fcb1576f
Duy Linh Nguyen <duylinhng95@gmail.com>
2019-01-25 13:50:09 +0700

    Add Schema AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer(['Post.sidebar'], PostComposer::class);
+        Schema::defaultStringLength(191);
     }
 
     /**
